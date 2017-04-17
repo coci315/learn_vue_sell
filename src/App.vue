f8aceda4d72c6474971a1488ca452b40c1f7e5f6
coci315 <coci315@gmail.com>
2017-04-17 22:04:29 +0800

    add header.vue

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header';
+
+const ERR_OK = 0;
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      const result = response.body;
+      if (result.errno === ERR_OK) {
+        this.seller = result.data;
+        console.log(this.seller);
+      }
+    });
+  },
+  components: {
+    'v-header': header
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+@import "./common/scss/mixin.scss";
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  .tab {
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    @include border-1px(rgba(7, 17, 27, .1));
+    .tab-item {
+      flex: 1;
+      text-align: center;
+      &>a {
+        display: block;
+        font-size: 14px;
+        color: rgb(77, 85, 93);
+        &.active {
+          color: rgb(240, 20, 20);
+        }
+      }
+    }
+  }
+}
+</style>
